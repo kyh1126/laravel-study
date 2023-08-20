f2f57af523224163f5fb97060519a69ea4f49db9
kyh1126 <kyh901126@nate.com>
2023-08-20 22:35:48 +0900

    [TIL/book] 라라벨 실전 웹 애플리케이션 개발 (3-2-3. 레이어드 아키텍처)
    - 컨트롤러상에 기술된 데이터베이스 조작 클래스

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,46 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Service\BookReviewService;
-use App\Service\UserService;
-use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Purchase;
 
 final class UserController extends Controller
 {
-    private $userService;
-    private $bookReviewService;
-    // 1
-    public function __construct(
-        UserService       $userService,
-        BookReviewService $bookReviewService
-    )
+    public function index(string $id)
     {
-        $this->userService = $userService;
-        $this->bookReviewService = $bookReviewService;
-    }
+        $user = User::find(intval($id));
+        $purchase = Purchase::findAllBy($user->id);
+        // 데이터베이스로부터 얻은 값을 사용한 처리 등
 
-    public function index(Request $request): View
-    {
-        return view('user.index', [
-            'user' => $this->userService->retrieveUser($request->get('id'))
-        ]);
-    }
-
-    public function store(Request $request): RedirectResponse
-    {
-        $this->userService->activate(
-            $request->get('user_id'),
-            $request->get('user_name')
-        );
-        // 2
-        $this->bookReviewService->addReview(
-            $request->get('user_id'),
-            $request->get('book_id'),
-            $request->get('review')
-        );
-        // 응답 반환 처리
-        return redirect('/users');
+        return view('user.index', ['user' => $user]);
     }
 }
