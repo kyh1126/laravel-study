8d709f2f1ec8419b99229341486046080ae63e4a
kyh1126 <kyh901126@nate.com>
2023-08-29 00:28:39 +0900

    [TIL/book] 라라벨 실전 웹 애플리케이션 개발 (4-2-5. 밸리데이션 - 규칙 커스터마이즈)
    - 2. 조건을 이용한 규칙 추가 - sometimes 메서드

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,19 +13,20 @@ final class UserController extends Controller
     {
         // 'name'의 밸리데이션 규칙에 'ascii_alpha' 추가
         $rules = [
-            'name' => ['required', 'max:20', 'ascii_alpha'],
+            'name' => ['required', 'max:20'],
             'email' => ['required', 'email', 'max:255'],
         ];
 
         $inputs = $request->all();
 
-        // 밸리데이션 규칙에 'ascii_alpha' 추가
-        Validator::extend('ascii_alpha', function ($attribute, $value, $parameters) {
-            // 영대소문자이면 true(밸리데이션 OK)
-            return preg_match('/^[a-zA-Z]+$/', $value);
-        });
-
         $validator = Validator::make($inputs, $rules);
+        $validator->sometimes(
+            'age',
+            'integer|min:18',
+            function ($inputs) {
+                return $inputs->mailmagazine === 'allow';
+            }
+        );
 
         if ($validator->fails()) {
             // 밸리데이션 에러 시 처리
