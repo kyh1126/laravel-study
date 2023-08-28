447b84a2520e2d491dea66c4e705f9d3ed342ad5
kyh1126 <kyh901126@nate.com>
2023-08-28 23:45:49 +0900

    [TIL/book] 라라벨 실전 웹 애플리케이션 개발 (4-2-4. 밸리데이션 - 밸리데이션 실패 처리)

--- app/Http/Requests/UserRegistPost.php
+++ app/Http/Requests/UserRegistPost.php
@@ -15,6 +15,17 @@ class UserRegistPost extends FormRequest
         return true; // 요청 취득을 기준으로 확인하므로 실행 허가로 변경
     }
 
+    public function messages()
+    {
+        return [
+            'name.required' => '이름을 입력해 주십시오',
+            'name.max' => '이름은 최대 20문자까지 입력할 수 있습니다',
+            'email.required' => '메일주소를 입력해 주십시오',
+            'email.email' => '메일주소 형식이 올바르지 않습니다',
+            'email.max' => '메일주소는 최대 255문자까지 입력할 수 있습니다',
+        ];
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
