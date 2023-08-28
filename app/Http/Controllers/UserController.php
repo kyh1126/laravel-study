d6e5cecc75301e985549e4d71c05c65bba62476c
kyh1126 <kyh901126@nate.com>
2023-08-29 00:22:55 +0900

    [TIL/book] 라라벨 실전 웹 애플리케이션 개발 (4-2-5. 밸리데이션 - 규칙 커스터마이즈)
    - 1. 규칙 추가 - extend 메서드

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,34 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UserRegistPost;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 final class UserController extends Controller
 {
-    public function register(UserRegistPost $request)
+    public function register(Request $request)
     {
-        // 여기에 도달할 때까지 밸리데이션 판정을 수행한다
+        // 'name'의 밸리데이션 규칙에 'ascii_alpha' 추가
+        $rules = [
+            'name' => ['required', 'max:20', 'ascii_alpha'],
+            'email' => ['required', 'email', 'max:255'],
+        ];
 
-        // 밸리데이션 통과 후의 처리
+        $inputs = $request->all();
+
+        // 밸리데이션 규칙에 'ascii_alpha' 추가
+        Validator::extend('ascii_alpha', function ($attribute, $value, $parameters) {
+            // 영대소문자이면 true(밸리데이션 OK)
+            return preg_match('/^[a-zA-Z]+$/', $value);
+        });
+
+        $validator = Validator::make($inputs, $rules);
+
+        if ($validator->fails()) {
+            // 밸리데이션 에러 시 처리
+        }
+
+        // 밸리데이션 통과 후 처리
         $name = $request->get('name');
     }
 }
