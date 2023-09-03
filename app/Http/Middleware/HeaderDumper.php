ecd3c3400ab3ec95bcb8b9476002773e722dd0b6
kyh1126 <kyh901126@nate.com>
2023-09-03 20:25:02 +0900

    [TIL/book] 라라벨 실전 웹 애플리케이션 개발 (4-4-3. 미들웨어 - 커스텀 미들웨어 구현)
    - 요청 헤더와 응답 헤더를 로그로 출력하는 미들웨어

---
+++ app/Http/Middleware/HeaderDumper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class HeaderDumper
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $this->logger->info(
+            'request',
+            [
+                'header' => strval($request->headers)
+            ]
+        );
+        // 헬퍼 함수 이용 시에는 다음과 같다
+        // info('request', ['header' => strval($request->headers)]);
+        $response = $next($request);
+        $this->logger->info(
+            'response',
+            [
+                'header' => strval($response->headers)
+            ]
+        );
+        return $response;
+    }
+}
