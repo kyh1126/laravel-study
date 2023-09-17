141ad30e7a66de50e6c176066e71d6d53d203623
kyh1126 <kyh901126@nate.com>
2023-09-17 19:00:28 +0900

    [TIL/book] 라라벨 실전 웹 애플리케이션 개발 (5-2-4. 시더 - Faker 이용)

--- database/seeders/AuthorsTableSeeder.php
+++ database/seeders/AuthorsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -10,12 +11,12 @@ class AuthorsTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        // Authors 테이블에 레코드를 10건 등록한다
+        // Faker를 사용해 Authors 테이블에 레코드를 10건 등록한다
         for ($i = 1; $i <= 10; $i++) {
             $author = [
-                'name' => 'author' . $i,
+                'name' => $faker->name,
                 'created_at' => now(),
                 'updated_at' => now(),
             ];
