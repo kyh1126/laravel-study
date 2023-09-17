ed9973fbe444a2ae2a47f0adc3b4f0f4730f4531
kyh1126 <kyh901126@nate.com>
2023-09-18 02:23:13 +0900

    [TIL/book] 라라벨 실전 웹 애플리케이션 개발 (5-3-2. Eloquent - 규약과 속성)

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     use HasFactory;
+
+    protected $table = 't_author';
+    protected $primaryKey = 'author_id';
+    public $timestamps = false;
+
+    // name 칼럼은 수정할 수 있도록 한다
+    protected $fillable = [
+        'name',
+    ];
+
+    // id, created_at, updated_at은 임의로 수정할 수 없도록 한다
+//    protected $guarded = [
+//        'id',
+//        'created_at',
+//        'updated_at',
+//    ];
 }
